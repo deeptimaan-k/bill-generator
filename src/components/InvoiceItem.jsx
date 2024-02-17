757f5e97a9ff3a411de1afea77c47f17c62a5a81
PyProdger <76729397+deeptimaan-k@users.noreply.github.com>
2024-02-17 22:18:24 +0530

    v1.0

---
+++ src/components/InvoiceItem.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import InvoiceField from './InvoiceField';
+
+const InvoiceItem = ({ id, name, qty, price, onDeleteItem, onEdtiItem }) => {
+  const deleteItemHandler = () => {
+    onDeleteItem(id);
+  };
+
+  return (
+    <tr>
+      <td className="w-full">
+        <InvoiceField
+          onEditItem={(event) => onEdtiItem(event)}
+          cellData={{
+            placeholder: 'Item name',
+            type: 'text',
+            name: 'name',
+            id: id,
+            value: name,
+          }}
+        />
+      </td>
+      <td className="min-w-[65px] md:min-w-[80px]">
+        <InvoiceField
+          onEditItem={(event) => onEdtiItem(event)}
+          cellData={{
+            type: 'number',
+            min: '1',
+            name: 'qty',
+            id: id,
+            value: qty,
+          }}
+        />
+      </td>
+      <td className="relative min-w-[100px] md:min-w-[150px]">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-2 top-1/2 h-6 w-6 -translate-y-1/2 text-gray-400 sm:left-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="m8.5 9.99984h7m-7-3.49984h7m-1.5 11.5002-5.5-4.5 1.5-.0002c4.4447 0 4.4447-7 0-7m12 5.5c0 5.5228-4.4772 10-10 10-5.52285 0-10-4.4772-10-10 0-5.52285 4.47715-10 10-10 5.5228 0 10 4.47715 10 10z"
+            
+          />
+        </svg>
+        <InvoiceField
+          onEditItem={(event) => onEdtiItem(event)}
+          cellData={{
+            className: 'text-right',
+            type: 'number',
+            min: '0.01',
+            step: '0.01',
+            name: 'price',
+            id: id,
+            value: price,
+          }}
+        />
+      </td>
+      <td className="flex items-center justify-center">
+        <button
+          className="rounded-md bg-red-500 p-2 text-white shadow-sm transition-colors duration-200 hover:bg-red-600"
+          onClick={deleteItemHandler}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default InvoiceItem;
